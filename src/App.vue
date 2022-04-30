07ed6f4bcfc5b58152bb822bb6784af3a75f1d43
Hilton Meyer <development@hiltonmeyer.com>
2022-04-30 10:25:56 +0300

    Added leaflet

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,103 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <div style="height: 90vh; width: 90vw">
+    <l-map
+      v-model="zoom"
+      v-model:zoom="zoom"
+      :center="[47.41322, -1.219482]"
+      @move="log('move')"
+      @click="addMarker"
+    >
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      ></l-tile-layer>
+      <l-control-layers />
 
+      <l-marker v-for="marker, index in markers" :key=index :lat-lng="marker" @click="removeMarker(index)"></l-marker>
+
+      <!-- <l-marker :lat-lng="[47.41322, -1.219482]">
+        <l-icon :icon-url="iconUrl" :icon-size="iconSize" />
+        <l-tooltip>Pinus Sylvestris</l-tooltip>
+        <l-popup>Pinus Sylvestris,Cool trunk </l-popup>
+      </l-marker> -->
+
+      <!-- <l-marker
+        :lat-lng="[50, 50]"
+        draggable
+        @moveend="log('moveend' + `{z},{x},{y}`)"
+      >
+        <l-popup>Can hold info of tree</l-popup>
+      </l-marker>
+
+      <l-marker
+        :lat-lng="[0, 0]"
+        draggable
+        @moveend="log('moveend' + `{z},{x},{y}`)"
+      >
+        <l-tooltip>Hover for short desc</l-tooltip>
+      </l-marker> -->
+    </l-map>
+    <!-- <button @click="changeIcon">New kitten icon</button> -->
+  </div>
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {
+  LMap,
+  // LIcon,
+  LTileLayer,
+  LMarker,
+  LControlLayers,
+  // LTooltip,
+  // LPopup,
+  // LPolyline,
+  // LPolygon,
+  // LRectangle,
+} from '@vue-leaflet/vue-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    LMap,
+    // LIcon,
+    LTileLayer,
+    LMarker,
+    LControlLayers,
+    // LTooltip,
+    // LPopup,
+    //LPolyline,
+    //LPolygon,
+    //LRectangle,
+  },
+  data() {
+    return {
+      zoom: 10,
+      iconWidth: 25,
+      iconHeight: 40,
+      markers: [
+        L.latLng(47.412, -1.218),
+        L.latLng(47.413220, -1.219482),
+        L.latLng(47.414, -1.22),
+      ]
+    };
+  },
+  computed: {
+    iconUrl() {
+      return `https://placekitten.com/${this.iconWidth}/${this.iconHeight}`;
+    },
+    iconSize() {
+      return [this.iconWidth, this.iconHeight];
+    },
+  },
+  methods: {
+    log(a) {
+      console.log(a);
+    },
+    changeIcon() {
+      this.iconWidth += 2;
+      if (this.iconWidth > this.iconHeight) {
+        this.iconWidth = Math.floor(this.iconHeight / 2);
+      }
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
