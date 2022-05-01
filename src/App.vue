88744a8e8ebbbf152075453146ce7a7a26826013
Hilton Meyer <development@hiltonmeyer.com>
2022-05-01 06:23:16 +0300

    Create uuid for fixing remove issue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div style="height: 90vh; width: 90vw">
     <l-map
-      v-model="zoom"
       v-model:zoom="zoom"
+      :min-zoom="minZoom"
+      :max-zoom="maxZoom"
       :center="user.defaultLocation"
       @move="log('move')"
       @click="addMarker"
@@ -18,7 +19,7 @@
         :lat-lng="marker.latLng"
         @click="removeMarker(marker.id)"
         draggable
-        >
+      >
         <l-tooltip>{{ marker.tooltip }}</l-tooltip>
         <l-icon :icon-url="marker.iconUrl" :icon-size="iconSize" />
       </l-marker>
@@ -80,23 +81,25 @@ export default {
   data() {
     return {
       zoom: 8,
+      minZoom: 2,
+      maxZoom: 19,
       iconWidth: 25,
       iconHeight: 25,
       markers: [
         {
-          id: 1,
+          id: Math.random().toString(36).slice(2),
           latLng: L.latLng(32.75, 35.5521),
           tooltip: 'Pinus Sylvestris',
           iconUrl: `https://placekitten.com/50/50`,
         },
         {
-          id: 2,
+          id: Math.random().toString(36).slice(2),
           latLng: L.latLng(32.55, 35.3545),
           tooltip: 'Acer Palmatum',
           iconUrl: `https://placekitten.com/50/50`,
         },
         {
-          id: 3,
+          id: Math.random().toString(36).slice(2),
           latLng: L.latLng(33.0451, 35.1578),
           tooltip: 'Ficus Binyamina',
           iconUrl: `https://placekitten.com/50/50`,
@@ -119,7 +122,7 @@ export default {
     addMarker(event) {
       if (!event.latlng) return;
       const newTree = {
-        id: this.markers.length + 1,
+        id: Math.random().toString(36).slice(2),
         latLng: event.latlng,
         tooltip: 'New Tree',
         iconUrl: '',
@@ -128,9 +131,9 @@ export default {
       this.markers.push(newTree);
       console.log(this.markers);
     },
-    removeMarker(index) {
-      console.log(index);
-      this.markers.splice(index - 1, 1);
+    removeMarker(id) {
+      console.log(id);
+      this.markers.splice(this.markers.findIndex(item => item.id === id), 1);
     },
   },
 };
