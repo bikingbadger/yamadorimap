3a65c17faa849ffc7b4f755ea7270e7ca847cbed
Hilton Meyer <development@hiltonmeyer.com>
2022-05-02 10:58:14 +0300

    add pinia

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import 'leaflet/dist/leaflet.css';
+import { createPinia } from 'pinia'
+
 
 const mapApp = createApp(App);
 
+// Add pinia for state
+mapApp.use(createPinia())
 
 
 mapApp.mount('#app');
