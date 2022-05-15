723aec034024ece89b045d77f8eaf8b0825206da
Hilton Meyer <development@hiltonmeyer.com>
2022-05-15 14:34:13 +0300

    Added vue router

--- src/main.js
+++ src/main.js
@@ -2,28 +2,29 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import 'leaflet/dist/leaflet.css';
 import { createPinia } from 'pinia';
+import router from './router';
 
 // Add  auth0
 import { domain, clientId, audience } from '../auth_config.json';
 import { Auth0Plugin } from '@/auth/auth0-plugin';
 
-const mapApp = createApp(App);
+const mapApp = createApp(App).use(router);
 
 // Add pinia for state
 mapApp.use(createPinia());
 
 // Install authentication plugin
 mapApp.use(Auth0Plugin, {
-    domain,
-    clientId,
-    audience,
-    onRedirectCallback: (appState)-> {
-        router.push(
-            appState && appState.targetUrl
-            ? appState.targetUrl
-            : window.location.pathname
-        )
-    }
-})
+  domain,
+  clientId,
+  audience,
+  onRedirectCallback: (appState) => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname,
+    );
+  },
+});
 
 mapApp.mount('#app');
