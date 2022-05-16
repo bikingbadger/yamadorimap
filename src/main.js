0fa59ba6e5196d7b20b406d9df48352b5b2fe51e
Hilton Meyer <development@hiltonmeyer.com>
2022-05-16 15:56:32 +0300

    Add Prime Vue for components

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import 'leaflet/dist/leaflet.css';
-import { createPinia } from 'pinia';
+
+// Vue router for routing
 import router from './router';
+
+// Leaflet CSS for maps
+import 'leaflet/dist/leaflet.css';
+
+// Add PrimeVue for CSS components
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/saga-blue/theme.css'   //theme
+import 'primevue/resources/primevue.min.css'           //core css
+import 'primeicons/primeicons.css'    
+import Button from 'primevue/button';        
+import InputText from 'primevue/inputtext';     //icons
+
+// Import Pinia for state
+import { createPinia } from 'pinia';
 
 // Add  auth0
 import { domain, clientId } from '../auth_config.json';
@@ -10,17 +24,24 @@ import { createAuth0 } from '@auth0/auth0-vue';
 
 const mapApp = createApp(App);
 
-// Add router
+// Use router
 mapApp.use(router);
 
-// Add pinia for state
+// Use Prime Vue
+mapApp.use(PrimeVue);
+mapApp.component('Button',Button)
+mapApp.component('InputText',InputText)
+
+// Use pinia for state
 mapApp.use(createPinia());
 
 // Install authentication plugin
-mapApp.use(createAuth0({
+mapApp.use(
+  createAuth0({
     domain: domain,
     client_id: clientId,
-    redirect_uri: window.location.origin
-}));
+    redirect_uri: window.location.origin,
+  }),
+);
 
 mapApp.mount('#app');
