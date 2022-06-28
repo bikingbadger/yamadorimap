0cc1d99a79d6838f7220290f45e562570db8a8e3
Hilton Meyer <development@hiltonmeyer.com>
2022-06-28 12:31:12 +0300

    [feat](map): show location from localhost api

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,54 +1,56 @@
 <template>
-  <div >
-    <l-map 
+  <div>
+    <l-map
       style="height: 80vh; width: 100%"
       :min-zoom="minZoom"
       v-model:zoom="zoom"
       :max-zoom="maxZoom"
-   :center="user.defaultLocation"
+      :center="user.defaultLocation"
     >
-     
-       <!--   @click="addMarker" -->
+      <!--   @click="addMarker" -->
       <l-tile-layer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       ></l-tile-layer>
       <l-control-layers />
-<!-- 
-      <l-marker
-        v-for="marker in markers"
-        :key="marker.id"
-        :lat-lng="marker.latLng"
-        @click="removeMarker(marker.id)"
-        draggable
-      >
-        <l-tooltip>{{ marker.tooltip }}</l-tooltip>
-        <l-icon :icon-url="marker.iconUrl" :icon-size="iconSize" />
-      </l-marker>
 
-      <l-marker
-        v-for="location in locations.allLocations"
+      <!-- <l-marker
+        v-for="location in locations"
         :key="location.id"
         :lat-lng="location.latLng"
-      ></l-marker> -->
+        @click="removelocation(location.id)"
+        draggable
+      >
+        <l-tooltip>{{ location.tooltip }}</l-tooltip>
+        <l-icon :icon-url="location.iconUrl" :icon-size="iconSize" />
+      </l-marker> -->
+
+      <l-marker
+        v-for="location in locations"
+        :key="location.id"
+        :lat-lng="location.latLng"
+      >
+        <l-tooltip>{{ location.tooltip }}</l-tooltip>
+        <l-icon :icon-url="location.iconUrl" :icon-size="iconSize" />
+      </l-marker>
     </l-map>
   </div>
 </template>
 
 <script setup>
-import { ref
- , reactive
- //, computed 
-} from 'vue';
+import { ref, reactive, computed } from 'vue';
 
+// Import the store
+import { storeToRefs } from 'pinia';
+import { useLocationStore } from '../store/location';
 
- import { latLng } from "leaflet/dist/leaflet-src.esm";
+import { latLng } from 'leaflet/dist/leaflet-src.esm';
 import {
   LMap,
-  // LIcon,
-   LTileLayer,
-  // LMarker,
-   LControlLayers,
-  // LTooltip,
+  LIcon,
+  LTileLayer,
+  LMarker,
+  LControlLayers,
+  LTooltip,
 
   // LPopup,
   // LPolyline,
@@ -56,16 +58,21 @@ import {
   // LRectangle,
 } from '@vue-leaflet/vue-leaflet';
 
-// Import the locations store
-// import { useLocationStore } from '../store/location';
-
- const zoom = ref(8);
+// Map related setup
+const zoom = ref(8);
 const minZoom = ref(2);
- const maxZoom = ref(19);
-// const iconWidth = ref(25);
-// const iconHeight = ref(25);
+const maxZoom = ref(19);
+const iconWidth = ref(25);
+const iconHeight = ref(25);
 
-// const locations = useLocationStore();
+const iconSize = computed(() => {
+  return [iconWidth.value, iconHeight.value];
+});
+
+// Location related setup
+const { locations } = storeToRefs(useLocationStore());
+const locationStore = useLocationStore();
+locationStore.getLocations();
 
 const user = reactive({
   defaultLocation: latLng(32.0461, 34.8516),
@@ -92,25 +99,6 @@ const user = reactive({
 //     1,
 //   );
 // }
-
-// const iconSize = computed(() => {
-//   return [iconWidth.value, iconHeight.value];
-// });
-
-// export default {
-//   components: {
-//     LMap,
-//     LIcon,
-//     LTileLayer,
-//     LMarker,
-//     LControlLayers,
-//     LTooltip,
-//     // LPopup,
-//     //LPolyline,
-//     //LPolygon,
-//     //LRectangle,
-//   },
-// };
 </script>
 
 <style></style>
--- src/store/location.js
+++ src/store/location.js
@@ -1,39 +1,39 @@
 import { defineStore } from 'pinia';
-import {latLng} from 'leaflet';
+import { latLng } from 'leaflet';
 
 export const useLocationStore = defineStore('locations', {
-  state: () => {
-    return {
-      /** @type {{ id: string, latLang: L.latLng, tooltip: string, iconUrl: string }[]} */
-      locations: [
-        {
-          id: Math.random().toString(36).slice(2),
-          latLng: latLng(32.75, 35.5521),
-          tooltip: 'Pinus Sylvestris',
-          iconUrl: `https://placekitten.com/50/50`,
-        },
-        {
-          id: Math.random().toString(36).slice(2),
-          latLng: latLng(32.55, 35.3545),
-          tooltip: 'Acer Palmatum',
-          iconUrl: `https://placekitten.com/50/50`,
-        },
-        {
-          id: Math.random().toString(36).slice(2),
-          latLng: latLng(33.0451, 35.1578),
-          tooltip: 'Ficus Binyamina',
-          iconUrl: `https://placekitten.com/50/50`,
-        },
-      ],
-      nextId: 0,
-    };
-  },
+  state: () => ({
+    /** @type {{ id: string, latLang: L.latLng, tooltip: string, iconUrl: string }[]} */
+    locations: [],
+    nextId: 0,
+  }),
   getters: {
     allLocations() {
       return this.locations;
     },
   },
   actions: {
+    async getLocations() {
+      console.log('location');
+      const result = await fetch('http://localhost:3000/api/coordinates');
+      console.log('location', result);
+      const data = await result.json();
+      console.log('location', JSON.stringify(data, null, 2));
+      const userLocations = [...data.data];
+      console.log('location', JSON.stringify(userLocations, null, 2));
+      this.locations = userLocations.map((location) => {
+        const parsedLatLng = location.latLng
+          .split(',')
+          .map((ll) => parseFloat(ll));
+        console.log(parsedLatLng);
+        return {
+          id: location.id,
+          latLng: latLng(parsedLatLng),
+          tooltip: location.tooltip,
+          iconUrl: location.iconUrl,
+        };
+      });
+    },
     addMarker(location, treeName) {
       if (!location.latlng) return;
       const newLocation = {
